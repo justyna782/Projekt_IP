edb58bb9555a564f234b41c70dfda8758b2e5474
Mateusz <mateuszpacan96@interia.eu>
2020-05-14 16:19:19 +0200

    Test

---
+++ app/src/main/java/com/example/learn_english/HomeActivity.java
@@ -0,0 +1,30 @@
+package com.example.learn_english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void goToFind(View view) {
+        if(view.getId() == R.id.findId)
+        {
+            //Do something Like starting an activity
+            Intent intent = new Intent(HomeActivity.this, FindActivity.class);
+            startActivity(intent);
+        }
+        else if(view.getId()==R.id.profile_image)
+        {
+            Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/learn_english/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.learn_english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prifile);
+    }
+}
