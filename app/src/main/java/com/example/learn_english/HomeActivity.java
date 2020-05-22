7f2ebda42f1d55e1c178470b7c491d4dfafe4a83
justyna782 <justyna.twardzik@interia.pl>
2020-05-22 11:41:43 +0200

    dodanie translatora

--- app/src/main/java/com/example/learn_english/HomeActivity.java
+++ app/src/main/java/com/example/learn_english/HomeActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,11 @@ public class HomeActivity extends AppCompatActivity {
         else if(view.getId()==R.id.profile_image)
         {
             Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        }
+        else if(view.getId()==R.id.translatorId)
+        {
+            Intent intent = new Intent(HomeActivity.this, Translator.class);
             startActivity(intent);
         }
     }
